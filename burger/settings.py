bfdf001c7ee4620a35c8861459b7ba46531276cf
marcos sousa <nvahost@gmail.com>
2026-09-19 11:45:35 -0300

    recuperacao de senha ok

--- burger/settings.py
+++ burger/settings.py
@@ -214,6 +214,7 @@ EMAIL_BACKEND = os.getenv(
 )
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('1', 'true', 'yes')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() in ('1', 'true', 'yes')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
